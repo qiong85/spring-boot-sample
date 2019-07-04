3b97f6e16a55605edc7b7c35a5087a2fb8d9a0f6
lqna <qliu_lq@163.com>
2019-07-05 00:56:31 +0800

    add select,update,delete student

--- src/main/java/com/lq/springbootsample/mapper/StudentMapper.java
+++ src/main/java/com/lq/springbootsample/mapper/StudentMapper.java
@@ -1,9 +1,7 @@
 package com.lq.springbootsample.mapper;
 
 import com.lq.springbootsample.pojo.Student;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,4 +17,16 @@ public interface StudentMapper {
     @Insert("INSERT INTO student (id,student_id,name,age,sex,birthday) " +
             "VALUES( #{id}, #{student_id}, #{name}, #{age}, #{sex}, #{birthday} )")
     void addStudent(Student student);
+
+    @Select("SELECT * FROM student WHERE id = #{id};")
+    Student selectStudentById(Integer id);
+
+    @Delete("DELETE FROM student where id=#{id};")
+    void deleteStudentById(Integer id);
+
+    @Update("UPDATE student SET student_id = #{student_id},name = #{name},age = #{age}," +
+            "sex = #{sex},birthday = #{birthday}," +
+            "createTime=#{createTime}, updateTime=#{updateTime}" +
+            "WHERE id = #{id}")
+    void updateStudent(Student student);
 }
